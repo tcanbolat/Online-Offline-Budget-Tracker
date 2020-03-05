66f1668fdef0d26feaeb826ca62b92ed027ce1d5
atcanbolat <kangalcanbolat@gmail.com>
2020-03-04 20:24:20 -0800

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,77 @@
-var CACHE_NAME = "budgetapp-cache";
 
-var urlsToCache = [
-    '/',
-    '/assets/css/style.css',
-    '/assets/js/index.js',
-    '/assets/js/db.js',
-    "/dist/bundle.js",
-  ];
 
-self.addEventListener("install", function(event) {
+
+
+const FILES_TO_CACHE = [
+  '/',
+  '/assets/css/style.css',
+  '/assets/js/index.js',
+  '/assets/js/db.js',
+  '/dist/bundle.js',
+];
+
+const STATIC_CACHE = "static-cache";
+const RUNTIME_CACHE = "runtime-cache";
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .open(STATIC_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", function(event) {
+
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        // return array of cache names that are old to delete
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+
+  // use cache first for all other requests for performance
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/index.html");
-        }
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
       });
     })
   );
