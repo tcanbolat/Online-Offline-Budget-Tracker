2baba6106a7f001940df09f08980ff7b4ade148e
atcanbolat <kangalcanbolat@gmail.com>
2020-03-07 15:43:43 -0800

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,15 @@ var urlsToCache = [
   '/assets/css/style.css',
   '/assets/js/index.js',
   '/assets/js/db.js',
+  '/manifest.json',
+  '/assets/images/icons/icon-72x72.png',
+  '/assets/images/icons/icon-96x96.png',
+  '/assets/images/icons/icon-128x128.png',
+  '/assets/images/icons/icon-144x144.png',
+  '/assets/images/icons/icon-152x152.png',
+  '/assets/images/icons/icon-192x192.png',
+  '/assets/images/icons/icon-384x384.png',
+  '/assets/images/icons/icon-512x512.png'
 ];
 
 
@@ -19,17 +28,32 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/index.html");
-        }
+
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
       });
     })
   );
 });
-
